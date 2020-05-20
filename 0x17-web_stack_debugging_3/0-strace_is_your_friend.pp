85a5f89b8f24ddec8d0c11fa3b55ab7f8d129f4a
steven-cruz <stevencruz.300008@gmail.com>
2020-05-20 19:46:25 +0000

    replace wrongly named file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Replace wrongly named file
 exac { 'mv class-wp-locale.php class-wp-locale.phpp':
+     command => "sed -i 's/phpp/php/g' wp-settings.php",
      path => ['/bin'],
-     cwd => '/var/www/html/wp-includes',
-
+     cwd => '/var/www/html/',
  }
