5bc93158ddedb99d16b8b98c65125b40c3407fb6
steven-cruz <stevencruz.300008@gmail.com>
2020-05-28 16:13:08 +0000

    reset conection

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # reset conection port 22
 exec { 'increase the uLimit of nginx':
-C:\Users\ASUS>sed -i \'s/15/15000/g\' /etc/default/nginx',
+sed -i \'s/15/15000/g\' /etc/default/nginx',
   path    => ['/bin']
 }
 exec { 'restart nginx':
